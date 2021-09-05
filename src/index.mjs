434402eae960c092e19a82ca45bb3046b3349680
bnslarry <604814861@qq.com>
2021-09-05 23:55:30 +0800

    Optimizations config

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,13 @@ import ssr from './ssr.mjs';
 const app = express();
 
 app.get('/', async (req, res, next) => {
+  const {html, ttRenderMs} = await ssr(`${req.protocol}://localhost:3000`, '#posts');
+  // Add Server-Timing! See https://w3c.github.io/server-timing/.
+  res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
+  return res.status(200).send(html); // Serve prerendered page as response.
+});
+
+app.get('/shopeefood', async (req, res, next) => {
   const {html, ttRenderMs} = await ssr(`${req.protocol}s://shopeefood.vn`);
   // Add Server-Timing! See https://w3c.github.io/server-timing/.
   res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
--- src/ssr.mjs
+++ src/ssr.mjs
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 const RENDER_CACHE = new Map();
 
-async function ssr(url) {
+async function ssr(url, rootElementTag = '#app') {
     if (RENDER_CACHE.has(url)) {
         return {html: RENDER_CACHE.get(url), ttRenderMs: 0};
     }
@@ -11,13 +11,53 @@ async function ssr(url) {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    // 1. Intercept network requests.
+    await page.setRequestInterception(true);
+
     try {
+        const stylesheetContents = {};
+
+        page.on('request', req => {
+            // 2. Ignore requests for resources that don't produce DOM
+            // (images, stylesheets, media).
+            const allowlist = ['document', 'script','stylesheet', 'xhr', 'fetch'];
+            if (!allowlist.includes(req.resourceType())) {
+              return req.abort();
+            }
+        
+            // 3. Pass through all other requests.
+            req.continue();
+          });
+
+        page.on('response', async resp => {
+            const responseUrl = resp.url();
+            const sameOrigin = new URL(responseUrl).origin === new URL(url).origin;
+            const isStylesheet = resp.request().resourceType() === 'stylesheet';
+            if (sameOrigin && isStylesheet) {
+                stylesheetContents[responseUrl] = await resp.text();
+            }
+        });
+
         // networkidle0 waits for the network to be idle (no requests for 500ms).
         // The page's JS has likely produced markup by this point, but wait longer
         // if your site lazy loads, etc.
         // 当在至少500ms的时间内没有超过0个网络连接时，导航就完成了。
         await page.goto(url, {waitUntil: 'networkidle0'});
-        await page.waitForSelector('#app'); // ensure #app exists in the DOM.
+
+        await page.waitForSelector(rootElementTag); // ensure #app exists in the DOM.
+
+        // Replace stylesheets in the page with their equivalent <style>.
+        await page.$$eval('link[rel="stylesheet"]', (links, content) => {
+            links.forEach(link => {
+                const cssText = content[link.href];
+                if (cssText) {
+                    const style = document.createElement('style');
+                    style.textContent = cssText;
+                    link.replaceWith(style);
+                }
+            });
+        }, stylesheetContents);
     } catch (err) {
         console.error(err);
         throw new Error('page.goto/waitForSelector timed out.');
