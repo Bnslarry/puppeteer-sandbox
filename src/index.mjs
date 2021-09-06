19e9bff94e2ac07e48d618cbfa4098e00af610a1
glow <yuhui.liu@shopee.com>
2021-09-06 18:05:30 +0800

    add cron job

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,17 @@
 import express from 'express';
-import ssr from './ssr.mjs';
+import { ssr, clearCache } from './ssr.mjs';
 
 const app = express();
 
 app.get('/', async (req, res, next) => {
   const {html, ttRenderMs} = await ssr(`${req.protocol}://localhost:3000`, '#posts');
+  // Add Server-Timing! See https://w3c.github.io/server-timing/.
+  res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
+  return res.status(200).send(html); // Serve prerendered page as response.
+});
+
+app.get('/test', async (req, res, next) => {
+  const {html, ttRenderMs} = await ssr(`${req.protocol}s://blog.cjw.design/blog/frontend/puppeteer-ssr`, '#root');
   // Add Server-Timing! See https://w3c.github.io/server-timing/.
   res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
   return res.status(200).send(html); // Serve prerendered page as response.
@@ -17,4 +24,15 @@ app.get('/shopeefood', async (req, res, next) => {
   return res.status(200).send(html); // Serve prerendered page as response.
 });
 
-app.listen(8080, () => console.log('Server started. Press Ctrl+C to quit'));
+app.listen(8080, () => {
+                          console.log('Server started. Press Ctrl+C to quit');
+                          console.log('init job');
+                          ssr(`https://shopeefood.vn`); 
+                          setInterval( async () => {
+                            clearCache()
+                            console.log('clear cache')
+                            console.log('Execu cron update job')
+                            let html = await ssr(`https://shopeefood.vn`); 
+                          }, 20000)
+                       }
+          );
--- src/ssr.mjs
+++ src/ssr.mjs
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 
 const RENDER_CACHE = new Map();
+let browserWSEndpoint = null;
 
 async function ssr(url, rootElementTag = '#app') {
     if (RENDER_CACHE.has(url)) {
@@ -9,35 +10,82 @@ async function ssr(url, rootElementTag = '#app') {
 
     const start = Date.now();
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    let html = null
+    if (!browserWSEndpoint) {
+        const browser = await puppeteer.launch();
+        browserWSEndpoint = await browser.wsEndpoint();
+        const page = await browser.newPage();
+        html = await puppetSSR(page, url, rootElementTag)
+    }
+    else {
+        console.info('Connecting to existing Chrome instance.');
+        const browser = await puppeteer.connect({browserWSEndpoint});
+        const page = await browser.newPage();
+        html = await puppetSSR(page, url, rootElementTag)
+    }
+    
+    const ttRenderMs = Date.now() - start;
+    console.info(`Headless rendered page in: ${ttRenderMs}ms`);
+    
+    RENDER_CACHE.set(url, html); // cache rendered page.
+    
+    return {html, ttRenderMs};
+}
 
-    // 1. Intercept network requests.
-    await page.setRequestInterception(true);
+async function puppetSSR(page, url, rootElementTag) {
+    await page.setUserAgent(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+     );
+
+    //const WHITE_LIST_RESOURE_TYPE = ['document', 'xhr', 'fetch', 'script', 'text/html', 'preflight'];
+    // 谷歌百度统计相关请求屏蔽
+    const BLACK_LIST_REQUEST_URL = [
+        'www.google-analytics.com',
+        '/gtag/js',
+        'ga.js',
+        'analytics.js',
+        'hm.baidu.com',
+        'hm.js',
+    ];
 
     try {
-        const stylesheetContents = {};
-
+        // 开启请求拦截器功能
+        await page.setRequestInterception(true);
         page.on('request', req => {
-            // 2. Ignore requests for resources that don't produce DOM
-            // (images, stylesheets, media).
-            const allowlist = ['document', 'script','stylesheet', 'xhr', 'fetch'];
-            if (!allowlist.includes(req.resourceType())) {
+            /* 非白名单资源请求直接抛弃 */
+            // if (!WHITE_LIST_RESOURE_TYPE.includes(req.resourceType())) {
+            //   return req.abort();
+            // }
+            /* 黑名单请求，例如统计代码抛弃 */
+            if (BLACK_LIST_REQUEST_URL.some(url => req.url().includes(url))) {
               return req.abort();
             }
-        
-            // 3. Pass through all other requests.
+            /* 其他请求正常运行 */
             req.continue();
           });
 
-        page.on('response', async resp => {
-            const responseUrl = resp.url();
-            const sameOrigin = new URL(responseUrl).origin === new URL(url).origin;
-            const isStylesheet = resp.request().resourceType() === 'stylesheet';
-            if (sameOrigin && isStylesheet) {
-                stylesheetContents[responseUrl] = await resp.text();
-            }
-        });
+        // const stylesheetContents = {};
+
+        // page.on('request', req => {
+        //     // 2. Ignore requests for resources that don't produce DOM
+        //     // (images, stylesheets, media).
+        //     const allowlist = ['document', 'script','stylesheet', 'xhr', 'fetch'];
+        //     if (!allowlist.includes(req.resourceType())) {
+        //       return req.abort();
+        //     }
+        
+        //     // 3. Pass through all other requests.
+        //     req.continue();
+        //   });
+
+        // page.on('response', async resp => {
+        //     const responseUrl = resp.url();
+        //     const sameOrigin = new URL(responseUrl).origin === new URL(url).origin;
+        //     const isStylesheet = resp.request().resourceType() === 'stylesheet';
+        //     if (sameOrigin && isStylesheet) {
+        //         stylesheetContents[responseUrl] = await resp.text();
+        //     }
+        // });
 
         // networkidle0 waits for the network to be idle (no requests for 500ms).
         // The page's JS has likely produced markup by this point, but wait longer
@@ -48,30 +96,28 @@ async function ssr(url, rootElementTag = '#app') {
         await page.waitForSelector(rootElementTag); // ensure #app exists in the DOM.
 
         // Replace stylesheets in the page with their equivalent <style>.
-        await page.$$eval('link[rel="stylesheet"]', (links, content) => {
-            links.forEach(link => {
-                const cssText = content[link.href];
-                if (cssText) {
-                    const style = document.createElement('style');
-                    style.textContent = cssText;
-                    link.replaceWith(style);
-                }
-            });
-        }, stylesheetContents);
+        // await page.$$eval('link[rel="stylesheet"]', (links, content) => {
+        //     links.forEach(link => {
+        //         const cssText = content[link.href];
+        //         if (cssText) {
+        //             const style = document.createElement('style');
+        //             style.textContent = cssText;
+        //             link.replaceWith(style);
+        //         }
+        //     });
+        // }, stylesheetContents);
     } catch (err) {
         console.error(err);
         throw new Error('page.goto/waitForSelector timed out.');
     }
     
     const html = await page.content(); // serialized HTML of page DOM.
-    await browser.close();
-    
-    const ttRenderMs = Date.now() - start;
-    console.info(`Headless rendered page in: ${ttRenderMs}ms`);
-    
-    RENDER_CACHE.set(url, html); // cache rendered page.
-    
-    return {html, ttRenderMs};
+    await page.close();
+    return html
 }
 
-export {ssr as default};
+function clearCache() {
+    RENDER_CACHE.clear();
+  }
+  
+export {ssr, clearCache};
